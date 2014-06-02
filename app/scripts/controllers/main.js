c3dbe39b7be445c23691e490f42328e0d76338c1
shouvikbhuiyan1990 <shouvikbhuiyan1990@gmail.com>
2014-06-02 16:15:51 +0530

    miliBhagat1.0

--- app/scripts/controllers/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/*global $:false */
-app.factory('childrenService', ['$firebase', function($firebase) {
-    var listRef = new Firebase('https://daycareapp.firebaseio.com/children');
-    return $firebase(listRef);
-}]);
-app.controller('MainCtrl', ['$scope','childrenService',function ($scope,service) {
-    service.$bind($scope, 'children');
-}]);
--- app/scripts/factory/dataFactory.js
+++ app/scripts/factory/dataFactory.js
@@ -12,6 +12,60 @@ app.factory('childrenService', ['$firebase', function($firebase) {
 }]);
 app.controller('ChildrenController', ['$scope','childrenService',function ($scope,service) {
     service.$bind($scope, 'children');
+
+    $scope.childTopickDrop=[];
+    $scope.childGender;
+    $scope.changeButtonColor=function(event){
+        $(event.target).toggleClass('active');
+    }
+    $scope.sortChild="age";
+    $scope.slectChild=function(event){
+        //console.debug(event);
+        var count=0;
+        var temp;
+        $(event.target).toggleClass('list-group-item-success');
+        $scope.childTopickDrop.push($(event.target).attr('id'));
+            for(var i=0;i<$scope.childTopickDrop.length;i++){
+                if($(event.target).attr('id')==$scope.childTopickDrop[i]){
+                    if(count==0){
+                    temp=i;
+                    }
+                count++;
+                if(count>1){
+                    $scope.childTopickDrop.splice(i,1);
+                    $scope.childTopickDrop.splice(temp,1);
+                }
+                }
+            }
+    };
+    $scope.broadcastMsg=function(){
+        $scope.babyMessage='';
+    };
+
+    $scope.pickUp=function(){
+        var selectedChildren=$scope.childTopickDrop;
+        if(selectedChildren.length!=0){
+            for(var i=0;i<selectedChildren.length;i++){
+                for(var j=0;j<$scope.children.childrenArr.length;j++){
+                    if(selectedChildren[i]==$scope.children.childrenArr[j].uid && $scope.children.childrenArr[j].picked=='NotPicked'){
+                        $scope.children.childrenArr[j].picked='picked';
+                    }
+                }
+            }
+        }
+    }
+
+    $scope.check=function(){alert($scope.childGender);}
+
+    $scope.$watch('childTopickDrop',function(){
+       if($scope.childTopickDrop.length==0){
+            $('.pickDrop').hide();
+        }
+        else if($scope.childTopickDrop.length>0)
+            {$('.pickDrop').show();
+        }
+
+    },true);
 }]);
 //binding parents data
 app.factory('parentsService', ['$firebase', function($firebase) {
